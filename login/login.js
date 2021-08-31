6ad15db66b4369431093238812936b6e420589bd
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-08-31 20:28:57 -0300

    Adicionei javascript pra surgir janela popup no cadastro com dados do usuário

--- login/login.js
+++ login/login.js
@@ -1,6 +1,9 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const cadastroButton = document.getElementById('cadastro');
+const nomeCompleto = document.querySelector('input#nomeCompleto').value;
+const enderecoEmail = document.querySelector('input#enderecoEmail').value;
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +12,7 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+cadastroButton.addEventListener('click', () => {
+	window.alert(`Bem vindo, ${nomeCompleto}! Seu endereço de e-mail foi confirmado como ${enderecoEmail}. O mentorama agradece seu cadastro!`)
+})
